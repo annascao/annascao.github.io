68ebf6bc4db122330a33dc95a1a2818f7aeef104
annascao <anna.shuqi@gmail.com>
2024-11-28 02:52:39 -0800

    updating

---
+++ js/typing.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var text = "Welcome to my website!"; // The text to type out
+    var typedTextElement = document.getElementById("typed-text");
+    var index = 0;
+
+    function type() {
+      if (index < text.length) {
+        typedTextElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(type, 50); // speed = 100ms
+      }
+    }
+
+    type(); // Start typing
+  });
